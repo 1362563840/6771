e26f6b9ea4c7789330768ca41131a12cfc610b17
Athos <z5134298@ad.unsw.edu.au>
2019-07-31 17:30:35 +1000

    w

--- assignments/dg/graph.cpp
+++ assignments/dg/graph.cpp
@@ -17,22 +17,3 @@ using std::unordered_set;
 using std::vector;
 using std::weak_ptr;
 
-typedef gdwg::Graph::Node
-{
-    N name;
-    // std::set< std::shared_ptr<Edge>, []( const shared_ptr<Edge> lhs, const shared_ptr<Edge> rhs ) {
-    //                                         return (
-    //                                             ( lhs.dest < rhs.dest ) ||
-    //                                             ( ( lhs.dest == rhs.dest ) && ( lhs.weight < rhs.weight ) )
-    //                                         );
-    //                                             }
-    // > outcoming;
-    std::set< std::shared_ptr<Edge>, EdgeComparator > outcoming;
-    std::set< std::shared_ptr<Edge>, EdgeComparator > incoming;
-}Node
-
-template<typename N, typename E>
-Graph( std::vector<N>::const_iterator start, std::vector<N>::const_iterator end )
-{
-    
-}
