24c2ea62afaf2862909beb9419da334fc596db29
Yifan Zhu <yifan.zhu.96@hotmail.com>
2019-07-31 12:19:51 +1000

    Update client.cpp

--- assignments/dg/client.cpp
+++ assignments/dg/client.cpp
@@ -7,30 +7,16 @@
 
 int main() {
   gdwg::Graph<std::string, int> g;
-  g.InsertNode("hello");
-  g.InsertNode("how");
-  g.InsertNode("are");
-  g.InsertNode("you?");
+  g.InsertNode("A");
+  g.InsertNode("B");
+  g.InsertNode("C");
+  g.InsertNode("D");
 
-  g.InsertEdge("hello", "how", 5);
-  g.InsertEdge("hello", "are", 8);
-  g.InsertEdge("hello", "are", 2);
+  g.InsertEdge("A", "B", 3);
+  g.InsertEdge("C", "B", 2);
+  g.InsertEdge("D", "B", 4);
 
-  g.InsertEdge("how", "you?", 1);
-  g.InsertEdge("how", "hello", 4);
-
-  g.InsertEdge("are", "you?", 3);
+  g.MergeReplace("A", "B");
 
   std::cout << g << '\n';
-
-  gdwg::Graph<std::string, int> g2{g};
-
-  std::cout << g2 << "\n";
-
-  // This is a structured binding.
-  // https://en.cppreference.com/w/cpp/language/structured_binding
-  // It allows you to unpack your tuple.
-  for (const auto& [from, to, weight] : g) {
-    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-  }
 }
